698b78faf911a6f875b79022dcd816deb0f07830
les-bres <les.bres.2@gmail.com>
2014-11-03 13:20:12 -0500

    setup begin

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.debug = True
+    app.run()
+
