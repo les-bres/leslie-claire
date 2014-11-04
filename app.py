70c4af374970b7fece042a1398bccda41c6dba03
les-bres <les.bres.2@gmail.com>
2014-11-04 15:09:43 -0500

    registration pg

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__=="__main__":
     app.debug = True
     app.run()
