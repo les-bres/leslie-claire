504e534f3a261b1f3d08a02d6cae8bbf48c17642
Claire Burghard <claireburghard@gmail.com>
2014-11-04 18:14:44 -0500

    working on utils

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from utils import 
 
 app = Flask(__name__)
 
@@ -10,9 +11,22 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/register")
+#register page
+@app.route("/register", method=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method=="GET":
+        return render_template("register.html", message = "")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        name = request.form["name"]
+        email = request.form["email"]
+        if (emailconfirmation(email) == True):
+            add_user(username,password,name,email)
+            return redirect(url_for("register")
+        else:
+            return render_template("register.html", message = "invalid email, please try again.")
+
 
 if __name__=="__main__":
     app.debug = True
