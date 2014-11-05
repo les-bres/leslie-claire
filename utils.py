cb83719e8da2e63c861580a1e028bfeeecc56cb8
Claire Burghard <claireburghard@gmail.com>
2014-11-04 22:05:24 -0500

    fixed regex stuff

--- utils.py
+++ utils.py
@@ -3,26 +3,29 @@ import re
 # one method that uses all other checking methods
 def confirmation(username,password,email,name):  
     answer = "good"
-    if username_confirmation(username) == False:
-        answer = "invalid"
-    if password_confirmation(password) == False:
-        answer = "invalid"
-<<<<<<< HEAD
-    if email_confirmation(email) == False:
-        answer = "invalid"
-    if name_confirmation(name) == False:
-        answer = "invalid"
+    username_msg = username_confirmation(username)
+    password_msg = password_confirmation(password)
+    email_msg = email_confirmation(email)
+    name_msg = name_confirmation(name)
+    if username_msg != "valid":
+        answer = username_msg
+    if password_msg != "valid":
+        answer = password_msg
+    if email_msg != "valid":
+        answer = email_msg
+    if name_msg != "valid":
+        answer = name_msg
     return answer
 
 def username_confirmation(username):
-    if re.match("((\w)|(\d)|[\.-_]){6,15}"):
+    if re.match("((\w)|(\d)|[\.-_]){6,15}",username):
         return "valid"
     else:
         return "Sorry, this is not a valid username. Please only use letters, periods, underscores, and hyphens in your username. Usernames must be between 6 and 15 characters."
 
 
 def password_confirmation(password):
-    if re.match("([a-z]|\d+){6,15}"):
+    if re.match("([a-z]|\d+){6,15}",password):
         return "valid"
     else:
         return "Sorry, this is not a valid password. Please use only lowercase letters and numbers in your password. Passwords must be between 6 and 15 characters." 
@@ -34,44 +37,7 @@ def email_confirmation(email):
         return "Sorry, this is not a valid email. Please make sure you enter a real email."
 
 def name_confirmation(name):
-    if re.match("(\w|\s){6,15}"):
-        return "valid"
-    else:
-        return "Sorry, this is not a valid username. Please use only letters and spaces in your username. Usernames must be between 6 and 15 characters."
-
-import re
-
-# one method that uses all other checking methods
-def confirmation(username, password,email,name):    
-=======
->>>>>>> FETCH_HEAD
-    if email_confirmation(email) == False:
-        answer = "invalid"
-    if name_confirmation(name) == False:
-        answer = "invalid"
-    return answer
-
-def username_confirmation(username):
-    if re.match("((\w)|(\d)|[\.-_]){6,15}"):
-        return "valid"
-    else:
-        return "Sorry, this is not a valid username. Please only use letters, periods, underscores, and hyphens in your username. Usernames must be between 6 and 15 characters."
-
-
-def password_confirmation(password):
-    if re.match("([a-z]|\d+){6,15}"):
-        return "valid"
-    else:
-        return "Sorry, this is not a valid password. Please use only lowercase letters and numbers in your password. Passwords must be between 6 and 15 characters." 
-
-def email_confirmation(email):
-    if re.match("[a-z]+@[a-z]+\.[a-z]+",email):
-        return "valid"
-    else:
-        return "Sorry, this is not a valid email. Please make sure you enter a real email."
-
-def name_confirmation(name):
-    if re.match("(\w|\s){6,15}"):
+    if re.match("(\w|\s){6,15}",name):
         return "valid"
     else:
         return "Sorry, this is not a valid username. Please use only letters and spaces in your username. Usernames must be between 6 and 15 characters."
