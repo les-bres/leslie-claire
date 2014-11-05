a7a94426fb445e454158badcc23eae5640f49823
les-bres <les.bres.2@gmail.com>
2014-11-04 19:29:04 -0500

    put everything back my working version...

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template, request, redirect
-from utils import add_user, emailconfirmation
+from utils import confirmation
+from flask import Flask, flash, render_template, request, redirect, url_for, session, escape
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client.proj
+users = db.users
+
+
+def add_user(username,password,name,email):
+    user = {
+        'username' : username,
+        'password' : password,
+        'email' : email,
+        'name' : name
+    }
+    return users.insert(user)
+
+def authenticate( username, passw ):
+    user = users.find_one({'username': username})
+    #if username does not exist
+    if user  == None:
+        return "username does not exist"
+    elif user['password'] != passw:
+        return "password and username do not match"
+        
+    return "match"
+
+
 @app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+    if request.method=="GET":
+        return render_template("index.html", msg=None)
+    else:
+        username = request.form["name"]
+        password = request.form["password"]
+        
+        msg = authenticate( username, password )
+        
+        if (msg == "match"):
+            return redirect(url_for('profile'))
+        else:
+            return render_template("index.html", msg=msg)
 
 @app.route("/about")
 def about():
@@ -15,17 +52,28 @@ def about():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method=="GET":
-        return render_template("register.html", message = "")
+        print "dog"
+        return render_template("register.html", msg = None)
+       
     else:
         username = request.form["username"]
         password = request.form["password"]
-        name = request.form["name"]
         email = request.form["email"]
-        if (emailconfirmation(email) == True):
-            add_user(username,password,name,email)
-            return redirect(url_for("register"))
+        name = request.form["name"]
+
+        msg = confirmation(username, password,email,name)
+        print msg
+        print "cat"
+        if (msg == "good"):
+            add_user(username,password,email)
+            return render_template("index.html", msg="registration successful, please log in")
         else:
-            return render_template("register.html", message = "invalid email, please try again.")
+            return render_template("register.html", msg=msg)
+
+
+@app.route("/profile")
+def profile():
+    return "you logged in"
 
 
 if __name__=="__main__":
--- utils.py
+++ utils.py
@@ -1,21 +1,14 @@
-from flask import Flask, flash, render_template, request, redirect, url_for, session, escape
-from pymongo import MongoClient
 import re
 
-client = MongoClient('localhost', 27017)
-db = client.proj
-users = db.users
+# one method that uses all other checking methods
+def confirmation(username, password,email,name):    
+    if email_confirmation(email) == False:
+        return "invalid email"
+    else:
+        return "good"
 
-def add_user(username,password,name,email):
-    user = {
-        'username' : username,
-        'password' : password,
-        'email' : email,
-        'name' : name
-    }
-    return users.insert(user)
-    
-def emailconfirmation(email):
+
+def email_confirmation(email):
     if re.match("[a-z]+@[a-z]+\.[a-z]+",email):
         return True
     else:
